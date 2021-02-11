e4b449efc2f2a91f4af51e9e7c6d42e4fca9d863
Toni <garcia.t20@gmail.com>
2021-02-11 16:25:03 +0100

    added different methods: imprimir, restarIntent, imprimirVides, hasGuanyat. Also some comments on code to understand better the job done

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -68,4 +68,45 @@ public class Tauler {
         this.palabraEndevinada = palabraEndevinada;
     }
 
+
+
+    public String imprimir() {
+        //Definimos salida con " " el cual significa que hay un valor. Que este valor sea la cadena vacia o no, no es relevante.
+        String salida = "";
+        for (int i = 0; i < palabraEndevinada.length; i++) {
+            //si palabraEndevinada es distinto a null imprimirá la posición 1,2 o la longitud de la palabra que hayamos asignado
+            if (!(palabraEndevinada[i] == (null))) {
+                //salida += palabraEndevinada[i];
+                System.out.print(palabraEndevinada[i]);
+//en caso contrario, imprimirá _
+            } else {
+                System.out.print("_");
+                //  salida += "_";
+            }
+        }
+        //hay que darle una salida a este metodo
+        return salida;
+    }
+
+    //lo que hace este metodo es restar los intentos --
+    public void restarIntent() {
+        Integer restarIntent=intents--;
+    }
+
+
+    public String imprimirVides() {
+        String vidilla = String.format("Et queden %s vides de %s", intents,vidas);
+        if (1 == intents) vidilla = String.format("Et queda %s vida de %s",intents, vidas);
+        return vidilla;
+    }
+
+    /*String.join: Concatena los elementos de la matriz especificada o los miembros de una colección,
+    usando el separador indicado entre todos los elementos o miembros. Además, lo que hace este metodo es igualar paraulaSecreta con palabraEndevinada */
+    public boolean hasGuanyat() {
+        if (String.valueOf(paraulaSecreta).equals(String.join("",palabraEndevinada))){
+            return true;
+        }else
+            return false;
+    }
+
 }
