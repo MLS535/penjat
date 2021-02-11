0f00df93ac228b8dc3139720ef93d88416964db1
Maite <mladaria@cifpfbmoll.eu>
2021-02-11 16:10:19 +0100

    Subo Atributos y constructor

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -1,5 +1,71 @@
 package com.jaume.penjat;
 
 public class Tauler {
+    private String letra;
+    private Integer vidas;
+    private String paraula;
+    private Integer intents;
+    private char[] paraulaSecreta;
+    private String[] palabraEndevinada;
+
+    public Tauler(){
+
+    }
+    public Tauler(String letra, Integer vidas, String paraula, Integer intents, char[] paraulaSecreta, String[] palabraEndevinada) {
+        this.letra = letra;
+        this.vidas = vidas;
+        this.paraula = paraula;
+        this.intents = intents;
+        this.paraulaSecreta = paraulaSecreta;
+        this.palabraEndevinada = palabraEndevinada;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
+
+    public Integer getVidas() {
+        return vidas;
+    }
+
+    public void setVidas(Integer vidas) {
+        this.vidas = vidas;
+    }
+
+    public String getParaula() {
+        return paraula;
+    }
+
+    public void setParaula(String paraula) {
+        this.paraula = paraula;
+    }
+
+    public Integer getIntents() {
+        return intents;
+    }
+
+    public void setIntents(Integer intents) {
+        this.intents = intents;
+    }
+
+    public char[] getParaulaSecreta() {
+        return paraulaSecreta;
+    }
+
+    public void setParaulaSecreta(char[] paraulaSecreta) {
+        this.paraulaSecreta = paraulaSecreta;
+    }
+
+    public String[] getPalabraEndevinada() {
+        return palabraEndevinada;
+    }
+
+    public void setPalabraEndevinada(String[] palabraEndevinada) {
+        this.palabraEndevinada = palabraEndevinada;
+    }
 
 }
